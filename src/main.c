bd22856c235cebac880ef3de3c73e9a849d48e61
Kareem Mohamed <61213045+KareemZafan@users.noreply.github.com>
2026-03-29 12:19:30 +0200

    This is the overall commit message

--- src/main.c
+++ src/main.c
@@ -1,11 +1,18 @@
-#include<stdio.h> 
-
+#include <stdio.h> 
+#include <math.h> 
 double add (double a, double b){
  return a + b; 
 }
 
 double subtract(double a , double b){
  return a - b; 
+}
+
+double divide(double a , double b){
+ if(b == 0) exist(-1); 
+
+ return a / b; 
+ 
 }
 
 
@@ -16,4 +23,6 @@ int main(void)
   
   printf("Sum = %d\n", add(a,b); 
   printf("Subtraction = %d\n", subtract(a,b)); 
+   printf("Division = %d\n", divide(a,b)); 
+ 
 }
