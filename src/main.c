243474701fce695618daa3619f8310d29f3d6409
Kareem Mohamed <kareem.mohamed@adfinis.com>
2026-03-29 12:10:11 +0200

    Updating main.c

--- src/main.c
+++ src/main.c
@@ -1,11 +1,14 @@
 #include<stdio.h> 
 
+double add (double a, double b){
+ return a + b; 
+}
 
 int main(void)
 { 
   int a = 10; 
   int b = 20; 
   
-  printf("Sum = %d\n", a + b); 
+  printf("Sum = %d\n", add(a,b); 
 
 }
